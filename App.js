cc799e6e199a09e903ecd9971bad16e6be033245
Anish Kumar <anish.diliyan@gmail.com>
2025-05-12 10:54:41 +0530

    react and react dom added

---
+++ App.js
@@ -0,0 +1,8 @@
+// createElement("tag name", "tag attribute", "child tags")
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello World from React!")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
